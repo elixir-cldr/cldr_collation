4c83016e90a175cc2f5b20197283eb95cef63bf3
Kip Cole <kipcole9@gmail.com>
2019-02-18 04:13:17 +0800

    Initial commit

---
+++ cldr_collation/lib/cldr_collation.ex
@@ -0,0 +1,18 @@
+defmodule CldrCollation do
+  @moduledoc """
+  Documentation for CldrCollation.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CldrCollation.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ cldr_collation/test/cldr_collation_test.exs
@@ -0,0 +1,8 @@
+defmodule CldrCollationTest do
+  use ExUnit.Case
+  doctest CldrCollation
+
+  test "greets the world" do
+    assert CldrCollation.hello() == :world
+  end
+end

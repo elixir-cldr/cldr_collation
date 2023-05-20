eedaa6350daad6b40800c701b453aa9c6b07be65
Benjamin Fox <benjaminfox@hey.com>
2023-05-20 15:00:48 +0200

    Support all of icu_collator's CollatorOptions in the NIF API

--- native/ex_cldr_collation/src/lib.rs
+++ native/ex_cldr_collation/src/lib.rs
@@ -1,36 +1,15 @@
+mod collator_opts;
+
 use icu_collator::*;
 use icu_locid::Locale;
-use rustler::{NifMap, NifResult, NifUnitEnum};
-
-#[derive(NifUnitEnum)]
-enum Casing {
-    Sensitive,
-    Insensitive,
-}
-
-#[derive(NifMap)]
-struct ComparisonOptions {
-    casing: Casing,
-}
-
-impl From<ComparisonOptions> for CollatorOptions {
-    fn from(opts: ComparisonOptions) -> Self {
-        let mut collator_options = CollatorOptions::new();
-
-        match opts.casing {
-            Casing::Insensitive => collator_options.strength = Some(Strength::Primary),
-            Casing::Sensitive => (),
-        }
-
-        collator_options
-    }
-}
+use collator_opts::CollatorOptions;
+use rustler::NifResult;
 
 #[rustler::nif]
 fn sort<'a>(
     locale_tag: &str,
     list: Vec<&'a str>,
-    opts: ComparisonOptions,
+    opts: CollatorOptions,
 ) -> NifResult<Vec<&'a str>> {
     let locale: Locale = locale_tag.parse().map_err(|_| rustler::Error::BadArg)?;
     let collator =

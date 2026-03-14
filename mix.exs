76492f7c38a2e274a8e3f4672c22adb36068dc1a
Kip Cole <kipcole9@gmail.com>
2026-03-15 08:42:11 +1100

    Set version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -1,11 +1,13 @@
 defmodule Cldr.Collation.MixProject do
   use Mix.Project
 
+  @version "1.0.0"
+
   def project do
     [
-      app: :cldr_collation,
-      version: "0.1.0",
-      elixir: "~> 1.19",
+      app: :ex_cldr_collation,
+      version: @version,
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       compilers: maybe_elixir_make() ++ Mix.compilers(),
       make_makefile: "c_src/Makefile",

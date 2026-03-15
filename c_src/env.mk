12d72b67c2004a0c81a9ac4d6a66d04226f053de
Kip Cole <kipcole9@gmail.com>
2026-03-15 11:27:48 +1100

    Fix env.mk

--- c_src/env.mk
+++ c_src/env.mk
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-ERTS_INCLUDE_DIR ?= /Users/kip/.asdf/installs/erlang/21.2.5/erts-10.2.3/include/
-ERL_INTERFACE_INCLUDE_DIR ?= /Users/kip/.asdf/installs/erlang/21.2.5/lib/erl_interface-3.10.4/include
-ERL_INTERFACE_LIB_DIR ?= /Users/kip/.asdf/installs/erlang/21.2.5/lib/erl_interface-3.10.4/lib
-=======
 ERTS_INCLUDE_DIR ?= /Users/kip/.local/share/mise/installs/erlang/28.3.1/erts-16.2/include/
 ERL_INTERFACE_INCLUDE_DIR ?= /Users/kip/.local/share/mise/installs/erlang/28.3.1/lib/erl_interface-5.6.2/include
 ERL_INTERFACE_LIB_DIR ?= /Users/kip/.local/share/mise/installs/erlang/28.3.1/lib/erl_interface-5.6.2/lib
->>>>>>> c/main
+
